1d9b182cbccedbb1f8c91fc979964bdf8196904f
viniciuscalligaris <vinicius.cesar7@hotmail.com>
2025-03-24 10:53:02 -0300

    Corrigi o código removendo o comentário da linha do assign

---
+++ somador.v
@@ -0,0 +1,7 @@
+module somador( // A definição do módulo significa que esse é um componente sendo criado em Verilog
+  input [3:0] a, // entrada de 4 bits
+  input [3:0] b, // entrada de 4 bits
+  output [3:0] soma // saída de 4 bits
+);
+
+endmodule // Aqui acaba o módulo somador
